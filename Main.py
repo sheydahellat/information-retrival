15bae2a33e27b3d1b32a20049b49020e2519ba27
sheyda <sheydahellat@gmail.com>
2021-12-22 12:22:21 +0330

    phase 2 completed

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
+import math
 import pickle
 from pathlib import Path
 from parsivar import FindStems
 import stopWords
 import openpyxl
+import itertools
+from itertools import islice
+
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
 punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 xlsx_file = Path('../IR1_7k_news.xlsx')
 wb_obj = openpyxl.load_workbook(xlsx_file)
-def find_freq():
-    freq = []
-    for item in pos_index:
-       freq.append(pos_index[item][0])
-    return freq
+score = [0] * 8000
 
 
 def normalize_query(words):
@@ -26,152 +26,95 @@ def normalize_query(words):
     for word in words:
         stemFree.append(my_stemmer.convert_to_stem(word))
     return stemFree
-def query_oneWord(word):
-    doc_ids = []
-    if not pos_index.get(word):
-        return doc_ids
 
-    dictionary = pos_index[word]
-    for k in sorted(dictionary[1], key=lambda k: len(dictionary[1][k]), reverse=True):
-        doc_ids.append(k)
-    return doc_ids
 
-def query_twoWord(word1,word2,distance):
+def index_elimination(normalized_query):
+    unique_list= []
     same = []
-    not_k = []
-    if not pos_index.get(word1) or pos_index.get(word2):
-        return same , not_k
-    for i in pos_index[word1][1]:
-        for j in pos_index[word2][1]:
-            if (i == j):
-                not_k.append(i)
-                for f in pos_index[word1][1][i]:
-                    for g in pos_index[word2][1][j]:
-                        if (f == g - distance):
-                            same.append(i)
-    same = list(set(same))
-    not_k = list(set(not_k))
-    not_k = list(set(not_k).difference(set(same)))
 
-    return same , not_k
+    for q in normalized_query:
+        if q not in unique_list:
+            unique_list.append(q)
 
-def intersection_lists(one_word_list,two_word_list):
+    for term in unique_list:
+        count_q_query = unique_list.count(term)
+        wqt = 1+math.log(count_q_query, 10)
+        idf = len(pos_index[term][1])
+        if(term not in pos_index):
+            continue
+        for d in pos_index[term][1]:
+            count = pos_index[term][1][d][0]
+            wd = (1 + math.log(count, 10)) * math.log(7562 / idf, 10)
+            score[d] += (wd*wqt)
 
-    same = []
-    i = set.intersection(*two_word_list)
-    same.append(i)
-    same.append(*two_word_list)
-    same.append(*one_word_list)
-    return same
+        li = []
+        for i in range(len(score)):
+            li.append([score[i], i])
+        li.sort()
+        sort_index = []
 
-def search_query(normalized_query):
-    same = []
-    if len(normalized_query) == 1:
-        # print(normalized_query)
-        same = query_oneWord(normalized_query[0])
-        if len(same) == 0:
-            print("notFound")
-            return same
-
-    elif len(normalized_query) == 2 :
-        same , not_k = query_twoWord(normalized_query[0],normalized_query[1],1)
-        # print(same)
-        word_one = query_oneWord(normalized_query[0])
-        word_two = query_oneWord(normalized_query[1])
-        same = same + not_k
-        for w in word_one:
-            if w not in same:
-                same.append(w)
-        for w in word_two:
-            if w not in same:
-                same.append(w)
-
-    elif len(normalized_query) == 3:
-        same = []
-        query_words = {}
-        not_k = {}
-        one_words = {}
-        i = 0
-        for j in range(0, len(normalized_query)):
-            one_words[j] = (query_oneWord(normalized_query[j]))
-        query_words[0], not_k[0] = (query_twoWord(normalized_query[0], normalized_query[1], 1))
-        query_words[1], not_k[1] = (query_twoWord(normalized_query[0], normalized_query[2], 2))
-        query_words[2], not_k[2] = (query_twoWord(normalized_query[1], normalized_query[2], 1))
-        # for i in range(1,len(normalized_query)):
-        #     query_words[i],not_k[i] = (query_twoWord(normalized_query[0], normalized_query[i], i))
-        intersect_one_two_three2 = list(set(query_words[0]).intersection(query_words[1]))
-        intersect_one_two_three = list(set(intersect_one_two_three2).intersection(query_words[2]))
-        same = intersect_one_two_three
-        same = same + query_words[0] + query_words[1] + query_words[2] + not_k[0] +not_k[1] + not_k[0] + one_words[0] + one_words[1] + one_words[2]
-        same = list(OrderedDict.fromkeys(same))
-    elif len(normalized_query) > 3:
-        same = []
-        not_k = {}
-        query_words = {}
-        one_words = {}
-        intersect = {}
-        i = 0
-        for j in range(0, len(normalized_query)):
-            one_words[j] = (query_oneWord(normalized_query[j]))
-        for i in range(1,len(normalized_query)):
-            query_words[i],not_k[i] = (query_twoWord(normalized_query[0], normalized_query[i], i))
-        for j in range(1, len(normalized_query)):
-            intersect[j] = list(set(one_words[j-1]).intersection(one_words[j]))
-            same = same + intersect[j]
-        # same = same + query_words[0] + query_words[1] + query_words[2] + not_k[0] + not_k[1] + not_k[0] + one_words[0] + \
-        #        one_words[1] + one_words[2]
-        same = list(OrderedDict.fromkeys(same))
-
-       # same = intersect_one_two_three
+        for x in li:
+            sort_index.append(x[1])
+        same = Reverse(sort_index)[0:10]
 
     return same
 
+def search_championList(normalized_query):
+    unique_list= []
+    same = []
 
+    for q in normalized_query:
+        if q not in unique_list:
+            unique_list.append(q)
+
+    for term in unique_list:
+        # print(pos_index[term][1])
+        count_q_query = normalized_query.count(term)
+        wqt = 1+math.log(count_q_query, 10)
+        if(term not in pos_index):
+            continue
+
+        for d in pos_index[term][1]:
+            # print(d)
+            wd = pos_index[term][1][d]
+            score[d] += (wd*wqt)
+
+
+        li = []
+        for i in range(len(score)):
+            li.append([score[i], i])
+        # print(li)
+        li.sort()
+        sort_index = []
+        # print(li)
+
+        for x in li:
+            sort_index.append(x[1])
+        same = Reverse(sort_index)[0:10]
+
+    return same
+
+def Reverse(lst):
+    new_lst = lst[::-1]
+    return new_lst
 
 
 
 #reading from file
-posindex_file = open("withstem.pkl", "rb")
+posindex_file = open("championList.pkl", "rb")
 pos_index = pickle.load(posindex_file)
 
 #get query & normalaizing
-while(True):
-
-    print("enter your query:")
-    query = input()
-    for p in punc:
-        query = query.replace(p, "")
-    sep_query=query.split()
-    normalized_query = normalize_query(sep_query)
-    print(normalized_query)
-    same = search_query(normalized_query)
-    for doc_id in same:
-        print(wb_obj.active.cell(doc_id + 1, 2).value)
-        print(wb_obj.active.cell(doc_id + 1, 3).value)
 
 
-# a = find_freq()
-# print(a)
-# a.sort(reverse=True)
-# plt.yscale('log')
-# plt.xscale('log')
-# plt.plot(a)
-# plt.yscale('log')
-# plt.xscale('log')
-# plt.show()
-
-# len = len(pos_index)
-# sum = 0
-# for i in pos_index:
-#     sum = sum + pos_index[i][0]
-#
-# print(len)
-# print(sum)
-
-#
-# plt.plot([102867,202964,305166,455227],[8650,12170,14700,20457])
-# plt.plot([102867,202964,305166,455227],[10885,15504,18842,26763])
-# plt.axis([0, 1000000, 0, 100000])
-# plt.yscale('log')
-# plt.xscale('log')
-# plt.show()
+print("enter your query:")
+query = input()
+for p in punc:
+    query = query.replace(p, "")
+sep_query=query.split()
+normalized_query = normalize_query(sep_query)
+# print(normalized_query)
+same = search_championList(normalized_query)
+for doc_id in same:
+    print(wb_obj.active.cell(doc_id + 1, 2).value)
+    print(wb_obj.active.cell(doc_id + 1, 3).value)
--- program.py
+++ program.py
@@ -5,6 +5,8 @@ from parsivar import Normalizer
 from parsivar import Tokenizer
 import stopWords
 import pickle
+import itertools
+import math
 
 xlsx_file = Path('../IR1_7k_news.xlsx')
 wb_obj = openpyxl.load_workbook(xlsx_file)
@@ -19,8 +21,8 @@ def preprocessing(line):
 
     my_tokenizer = Tokenizer()
     words = my_tokenizer.tokenize_words(my_normalizer.normalize(line))
-    my_stemmer = FindStems()
-    stemFree = []
+    # my_stemmer = FindStems()
+    # stemFree = []
 
     for word in stopWords.stopWords:
         for token in words:
@@ -49,21 +51,38 @@ for row in sheet.iter_rows():
             pos_index[term][0] = pos_index[term][0] + 1
 
             if fileno in pos_index[term][1]:
-                pos_index[term][1][fileno].append(pos)
+                pos_index[term][1][fileno] += 1
 
             else:
-                pos_index[term][1][fileno] = [pos]
+                pos_index[term][1][fileno] = 1
         else:
 
             pos_index[term] = []
             pos_index[term].append(1)
             pos_index[term].append({})
-            pos_index[term][1][fileno] = [pos]
+            pos_index[term][1][fileno] = 1
     fileno += 1
+def make_championList():
+    unique_list= []
+    same = []
+
+    for term in pos_index:
+        idf = len(pos_index[term][1])
+        for d in pos_index[term][1]:
+            count = pos_index[term][1][d]
+            wd = (1 + math.log(count, 10)) * math.log(7562 / idf, 10)
+            pos_index[term][1][d] = wd
+        pos_index[term][1] = dict(sorted(pos_index[term][1].items(), key=lambda item: item[1], reverse=True))
+        pos_index[term][1] = dict(itertools.islice(pos_index[term][1].items(), 10))
 
 
+# print(pos_index['حسین'])
 # print(fileno)
-posindex_file = open("withstem.pkl", "wb")
+posindex_file = open("phase2.pkl", "wb")
 pickle.dump(pos_index, posindex_file)
 posindex_file.close()
 
+make_championList()
+championList_file = open("championList.pkl", "wb")
+pickle.dump(pos_index, championList_file)
+championList_file.close()
